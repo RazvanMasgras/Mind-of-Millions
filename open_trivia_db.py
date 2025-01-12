a81cb2da681dfd20a44a82c4efdf929dd60ecb9b
SerbanAlex04 <alex.serban22@icloud.com>
2025-01-12 23:14:56 +0200

    lifelines graphics

--- open_trivia_db.py
+++ open_trivia_db.py
@@ -187,21 +187,6 @@ def audience_poll(question):
             else:
                 weights[answer] = percentage
 
-    labels = ['A', 'B', 'C', 'D']
-    plt.bar(labels, weights.values())
-
-    for i, (key, value) in enumerate(weights.items()):
-        plt.text(i, value + 1, str(value), ha='center', color='white')
-
-    plt.gca().axes.get_yaxis().set_visible(False)
-
-    for spine in plt.gca().spines.values():
-        spine.set_visible(False)
-
-    plt.gca().tick_params(axis='x', colors='white')
-
-    plt.savefig("audience_poll.png", transparent=True)
-
     return weights
 
 
